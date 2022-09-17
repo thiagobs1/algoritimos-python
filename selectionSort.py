17b23e90714dc6cb3f0b385f874f231517de198a
Thiago Bastos da Silva <thiagobastos71@gmail.com>
2022-09-16 22:32:48 -0300

    Adiciona a ordenação por seleção

---
+++ selectionSort.py
@@ -0,0 +1,17 @@
+def buscaMenor(arr):
+    menor = arr[0]
+    indiceMenor = 0
+    for i in range(1, len(arr)):
+        if arr[i] < menor:
+            menor = arr[i]
+            indiceMenor = i
+    return indiceMenor
+
+def selectionSort(arr):
+    novoArr = []
+    for i in range (len(arr)):
+        menor = buscaMenor(arr)
+        novoArr.append(arr.pop(menor)) # adiciona o menor ao novoo array ao mesmo tempo em que exclui o menor do array anterior
+    return novoArr
+
+print(selectionSort([5,3,6,2,10]))
