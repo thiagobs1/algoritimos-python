29a6be123253cbf5f84b3c80171fb5e858bdee14
Thiago Bastos da Silva <thiagobastos71@gmail.com>
2022-09-16 22:34:19 -0300

    Adiciona um comentario sobre a notação bigO do algoritimo

--- selectionSort.py
+++ selectionSort.py
@@ -14,4 +14,4 @@ def selectionSort(arr):
         novoArr.append(arr.pop(menor)) # adiciona o menor ao novoo array ao mesmo tempo em que exclui o menor do array anterior
     return novoArr
 
-print(selectionSort([5,3,6,2,10]))
+print(selectionSort([5,3,6,2, 10, 1, 9, 20])) # tempo de execução = O(n^2) pois precisamos percorrer n elementos n vezes, desconsiderando a contante 1/2
